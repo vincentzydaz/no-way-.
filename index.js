a85bf5cf443788a5a3607b2ce7813dfc9f1ad7b2
vincentzydaz <vincentzydaz@gmail.com>
2024-10-30 01:07:51 +0800

    Update index.js

--- index.js
+++ index.js
@@ -101,7 +101,7 @@ async function loadBot() {
   executeBot("node", [SCRIPT_PATH]).catch(console.error);
 }
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   loadBot();
