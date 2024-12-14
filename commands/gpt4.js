47449bb6caa00930382c5942152b667f1b7e793a
vincentzydaz <vincentzydaz@gmail.com>
2024-12-14 23:21:42 +0800

    Update gpt4.js

--- commands/gpt4.js
+++ commands/gpt4.js
@@ -12,8 +12,8 @@ module.exports = {
     if (!prompt) return sendMessage(senderId, { text: "Usage: gpt4 <question>" }, pageAccessToken);
 
     try {
-      const { data: { response } } = await axios.get(`https://api.joshweb.click/api/gpt-4o?text=${encodeURIComponent(prompt)}`);
-      sendMessage(senderId, { text: response }, pageAccessToken);
+      const { data: { result } } = await axios.get(`https://api.joshweb.click/api/gpt-4o?q=${encodeURIComponent(prompt)}&uid=${senderId}`);
+      sendMessage(senderId, { text: result }, pageAccessToken);
     } catch {
       sendMessage(senderId, { text: 'There was an error generating the content. Please try again later.' }, pageAccessToken);
     }
